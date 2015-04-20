0919108de820e637b4eb3776741cba28bea60967
Dmitriy Trubenkov <403rus@gmail.com>
2015-04-20 23:22:28 +0300

    Fix ctrl-tab for tmux, add tmux to install

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ git submodule update --init --recursive
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 cp .bashrc ~/.bashrc
 cp ./home/.vimrc ~/.vimrc
+cp ./home/.tmux.conf ~/.tmux.conf
 cp -r ./home/.vim ~/
 vim +BundleInstall +qall
