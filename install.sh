1c1578c483c8f6ce8c1ce2575deb83621a66e24a
Dmitriy T <403rus@gmail.com>
2015-04-22 01:47:31 +0300

    Add alias fix for tmux+bash

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 git submodule update --init --recursive
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 cp .bashrc ~/.bashrc
+cp .bash_profile ~/
 cp ./home/.vimrc ~/.vimrc
 cp ./home/.tmux.conf ~/.tmux.conf
 cp -r ./home/.vim ~/
