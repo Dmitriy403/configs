a3441aadefe9c1c60d5cc3558ed91545aeba4498
Dmitriy Trubenkov <403rus@gmail.com>
2019-04-05 18:52:55 +0300

    Added tpm and plugins for tmux

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+#install tmux, vim
+sudo apt-get install tmux
+sudo apt-get install vim
+
 if ! grep -q "403scriptMagic" ~/.bashrc; then
     sudo cat .bashrc.patch >>  ~/.bashrc
     echo "bashrc patched"
@@ -10,3 +14,9 @@ if [ -d ~/.vim/ ]; then
     [ -d ~/.vim/colors/ ] || mkdir ~/.vim/colors/
     cp ./wombat256mod.vim ~/.vim/colors/
 fi
+
+if [ ! -d ~/.tmux/plugins/tpm/ ]; then
+    echo "clone tpm"
+    git clone git@github.com:tmux-plugins/tpm.git  ~/.tmux/plugins/tpm
+fi
+
