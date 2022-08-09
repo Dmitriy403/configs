eca62c6be1cc970f263876fc30c3aa4da8e81de1
Dmitrii T <403rus@gmail.com>
2022-08-09 19:55:57 +0300

    Add nvim installation, use homebrew for installation, use vim-plug

--- home/.vimrc
+++ home/.vimrc
@@ -81,3 +81,22 @@ else
     colorscheme desert
 endif
 
+"PLUGINS
+call plug#begin()
+
+" main one
+Plug 'ms-jpq/coq_nvim', {'branch': 'coq'}
+" 9000+ Snippets
+Plug 'ms-jpq/coq.artifacts', {'branch': 'artifacts'}
+
+" lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
+" Need to **configure separately**
+
+Plug 'ms-jpq/coq.thirdparty', {'branch': '3p'}
+" - shell repl
+" - nvim lua api
+" - scientific calculator
+" - comment banner
+" - etc
+
+call plug#end()
--- install.sh
+++ install.sh
@@ -1,22 +1,39 @@
+#force exit if anything goes wrong
+set -e
 #install tmux, vim
-sudo apt-get install tmux
-sudo apt-get install vim
+brew install tmux
+brew install nvim
+# remove bad tags:
+# brew uninstall ctags
+# install good tags:
+brew install universal-ctags
+apt install --yes -- python3-venv
 
-if ! grep -q "403scriptMagic" ~/.bashrc; then
-    sudo cat .bashrc.patch >>  ~/.bashrc
-    echo "bashrc patched"
-fi
+#if ! grep -q "403scriptMagic" ~/.bashrc; then
+#    sudo cat .bashrc.patch >>  ~/.bashrc
+#    echo "bashrc patched"
+#fi
 
 cp ./home/.vimrc ~/.vimrc
 cp ./home/.tmux.conf ~/.tmux.conf
 
-if [ -d ~/.vim/ ]; then
-    [ -d ~/.vim/colors/ ] || mkdir ~/.vim/colors/
-    cp ./wombat256mod.vim ~/.vim/colors/
-fi
 
-if [ ! -d ~/.tmux/plugins/tpm/ ]; then
-    echo "clone tpm"
-    git clone git@github.com:tmux-plugins/tpm.git  ~/.tmux/plugins/tpm
-fi
+#install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+#install coq_nvim
+
+#if [ -d ~/.vim/ ]; then
+#    [ -d ~/.vim/colors/ ] || mkdir ~/.vim/colors/
+#    cp ./wombat256mod.vim ~/.vim/colors/
+#fi
+
+#if [ ! -d ~/.tmux/plugins/tpm/ ]; then
+#    echo "clone tpm"
+#    git clone git@github.com:tmux-plugins/tpm.git  ~/.tmux/plugins/tpm
+#fi
+
+#install deps for vim/nvim plugins
+
+#curl -fsSL https://deno.land/x/install/install.sh | sh
