ae847b744b2ce1160a84c02ec1d57b44eabaacd9
Dmitrii T <403rus@gmail.com>
2022-08-09 19:55:57 +0300

    Use homebrew for installation, use vim-plug, start support for MacOS

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #force exit if anything goes wrong
 set -e
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo ${machine}
+
 #install tmux, vim
 brew install tmux
 brew install nvim
@@ -7,7 +18,15 @@ brew install nvim
 # brew uninstall ctags
 # install good tags:
 brew install universal-ctags
-apt install --yes -- python3-venv
+if [ "$machine" == "Linux" ]; then
+    sudo apt install --yes -- python3-venv
+else
+    if ["$machine" != "Mac"]; then
+       echo "Unexpected OS type"
+       exit 1
+    fi
+    pip3 install virtualenv
+fi
 
 #if ! grep -q "403scriptMagic" ~/.bashrc; then
 #    sudo cat .bashrc.patch >>  ~/.bashrc
@@ -21,19 +40,4 @@ cp ./home/.tmux.conf ~/.tmux.conf
 #install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-#install coq_nvim
-
-#if [ -d ~/.vim/ ]; then
-#    [ -d ~/.vim/colors/ ] || mkdir ~/.vim/colors/
-#    cp ./wombat256mod.vim ~/.vim/colors/
-#fi
-
-#if [ ! -d ~/.tmux/plugins/tpm/ ]; then
-#    echo "clone tpm"
-#    git clone git@github.com:tmux-plugins/tpm.git  ~/.tmux/plugins/tpm
-#fi
-
-#install deps for vim/nvim plugins
-
 #curl -fsSL https://deno.land/x/install/install.sh | sh
